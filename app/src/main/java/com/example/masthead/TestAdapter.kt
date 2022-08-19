9e7e5e858ce1fd0483ff0eeef259789e61ec602f
sambuddha.dhar <sambuddha.dhar@swiggy.in>
2022-08-19 13:23:38 +0530

    Add test3 fun

--- app/src/main/java/com/example/masthead/TestAdapter.kt
+++ app/src/main/java/com/example/masthead/TestAdapter.kt
@@ -31,6 +31,7 @@ class TestAdapter(val itemList: List<String>) : RecyclerView.Adapter<TestAdapter
     override fun getItemCount(): Int = itemList.size
     override fun test(): String = ""
     override fun test1(): Int = 1
-    override fun test2(): Float = 1F
+    override fun test2(): Int = 1
+    override fun test3(): String = ""
 
 }
